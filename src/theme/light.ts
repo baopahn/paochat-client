04892d92f9f12d38a2139d237af9b4423763ea45
PLHB1712289 <phanlehoaibaok10@gmail.com>
2021-07-18 16:44:04 +0700

    change theme

--- src/theme/light.ts
+++ src/theme/light.ts
@@ -1,11 +1,15 @@
 import { ThemeType } from "./types";
 
+// primary: "hsl(15, 96%, 54%)",
+// secondary: "hsl(15, 96%, 60%)",
+
 export const light: ThemeType = {
   isDark: false,
-  primary: "hsl(15, 96%, 54%)",
-  secondary: "hsl(15, 96%, 60%)",
+  primary: "hsl(206, 70%, 30%)",
+  secondary: "hsl(206, 70%, 30%)",
   text: "#050505",
-  background: "#fff",
+  // background: "#f8f8f8",
+  background: "hsl(210,100%,96%,0.25)",
   button: "rgba(0,0,0,0.1)",
   border: "rgba(0,0,0,0.05)",
   icon: "rgba(0,0,0,0.5)",
